7f80baa375497bd16b536f5da9aa94586b067bed
LuxSiteCZ <krulkostudio@gmail.com>
2026-08-28 03:09:19 +0200

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import { glob } from 'glob';
+import { resolve } from 'path';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
@@ -13,7 +14,7 @@ export default defineConfig(({ command }) => {
     build: {
       sourcemap: true,
       rollupOptions: {
-        input: glob.sync('./src/*.html'),
+        input: glob.sync(resolve(import.meta.dirname, 'src/*.html')),
         output: {
           manualChunks(id) {
             if (id.includes('node_modules')) {
